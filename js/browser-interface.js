e5bb6e47eaf93cdabb530a797bdc72d834c7b5e3
michael <michael.vang.1985@gmail.com>
2016-03-18 11:59:28 -0700

    parse more information from ajax request

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -21,16 +21,21 @@ $(document).ready(function(){
 
     $('.results').empty().append("<img class='avatar' src='" + avatar + "'><li>name: <a target='_blank' href='" + user_url + "'>" + name + "</a></li><li>number of repositories: <a target='_blank' href='" + repos_url + "'>" + repos + "</a></li><hr><ol class='repo_results'></ol>");
 
-    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + "&per_page=2").then(function(repo_item){
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + "&per_page=25").then(function(repo_item){
 
       $.each(repo_item, function(i) {
         var repo_name = repo_item[i].name;
         var repo_createdate = repo_item[i].created_at;
         var repo_url = repo_item[i].html_url;
+        var repo_description = repo_item[i].description;
+        if(repo_description === ""){
+          repo_description = "<em>no description provided</em>";
+        }
         console.log(repo_name);
         console.log(repo_createdate);
         console.log(repo_url);
-        $('.repo_results').append("<li><a target='_blank' href='" + repo_url + "'>" + repo_name + "</li>");
+        console.log(repo_item);
+        $('.repo_results').append("<li><a target='_blank' href='" + repo_url + "'>" + repo_name + "</a><p>date created: " + repo_createdate + "</p><p>description: " + repo_description + "</p></li>");
       });
     });
   }).fail(function(error){
