30fd407769b60b13c4ee60ab07b6c746d54da3d7
michael <michael.vang.1985@gmail.com>
2016-03-18 09:38:16 -0700

    parse .get object for information

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,9 +3,35 @@ var getRepos = require('./../js/getRepos-interface.js').getRepos;
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
-  $('#username').submit(function(event){
+
+  var reposArray = [];
+
+  $.get('https://api.github.com/users/michaellor?access_token=' + apiKey).then(function(response){
+    console.log(response);
+
+    var name = response.name;
+    var avatar = response.avatar_url;
+    var repos = response.public_repos;
+    reposArray.push(repos);
+    console.log(name)
+    console.log(avatar)
+    console.log(repos)
+    console.log(reposArray)
+    $('.results').append("<li>username: " + name + " <img class='avatar' src='" + avatar + "'></li><li>number of repositories: " + repos + "</li>");
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+
+
+  $('#userinput').submit(function(event){
     event.preventDefault();
-  })
+
+  var username = $('#username').val();
+  var repos = getRepos(username);
+
+  });
 
 
 });
