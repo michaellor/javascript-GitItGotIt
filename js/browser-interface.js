20638d8e44a27324fd91d8cf69fbe3ad5e634b84
michael <michael.vang.1985@gmail.com>
2016-03-18 08:37:48 -0700

    set up index.html forms

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,9 +1,17 @@
-var Class = require('./../js/class.js').Class;
-var moment = require('moment');
+// var Class = require('./../js/class.js').Class;
+// var moment = require('moment');
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
 
 
 
+});
+
+exports.getRepos = function(){
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
   });
+};
