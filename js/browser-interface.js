54eb59825871970d476b132a0f75be27ca5909d1
michael <michael.vang.1985@gmail.com>
2016-03-18 11:38:59 -0700

    link to user url and repository url

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -11,28 +11,28 @@ $(document).ready(function(){
   var username = $('#username').val();
 
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    console.log(response)
 
     var name = response.name;
     var avatar = response.avatar_url;
     var repos = response.public_repos;
+    var user_url = response.html_url;
+    var repos_url = response.repos_url;
 
-    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(repo_item){
+    $('.results').empty().append("<img class='avatar' src='" + avatar + "'><li>name: <a target='_blank' href='" + user_url + "'>" + name + "</a></li><li>number of repositories: <a target='_blank' href='" + repos_url + "'>" + repos + "</a></li><hr><ol class='repo_results'></ol>");
 
-      $.each(repo_item, function(i, item) {
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + "&per_page=2").then(function(repo_item){
 
+      $.each(repo_item, function(i) {
         var repo_name = repo_item[i].name;
         var repo_createdate = repo_item[i].created_at;
         var repo_url = repo_item[i].html_url;
         console.log(repo_name);
         console.log(repo_createdate);
         console.log(repo_url);
+        $('.repo_results').append("<li><a target='_blank' href='" + repo_url + "'>" + repo_name + "</li>");
       });
     });
-
-    $('.results').append("<img class='avatar' src='" + avatar + "'><li>name: " + name + "</li><li>number of repositories: " + repos + "</li><hr>");
-
-    $('.repo_results').append("public repositories:" + repo_name);
-
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
